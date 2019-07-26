e23319a91489a98bec9680c6b18f047a12b8a286
Jared Miller <jtmiller@protonmail.com>
2019-07-26 14:37:19 -0400

    Buncha search shit

--- tools/search.sh
+++ tools/search.sh
@@ -3,6 +3,11 @@
 # searches the filetype & keyword
 # in case of multiple filetypes (javascript.jsx) we just use the first
 
-# firefox "https://google.com/search?btnI=&q=$(echo $1| cut -d'.' -f1)+%22$2%22%20!"
+# firefox "https://google.com/search?q=$(echo $1| cut -d'.' -f1)+%22$2%22"
 # firefox "https://google.com/search?btnI=&q=${1//[.]/+}+$2"
-firefox "https://duckduckgo.com/?q=${1//[.]/+}+%22$2%22+!"
+
+# firefox "http://www.google.com/search?hl=en&q=${1//[.]/+}+$2"
+# firefox "http://www.google.com/search?hl=en&q=${1//[.]/+}+$2+mdn&btnI=I"
+
+# firefox "https://duckduckgo.com/?q=${1//[.]/+}+%22$2%22+!"
+firefox "https://duckduckgo.com/?q=${1//[.]/+}+%22$2%22"
